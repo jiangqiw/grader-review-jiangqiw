9469fccda5aeab759f39d2b993327788ccb06e8f
Jim Wu <129979991+jiangqiw@users.noreply.github.com>
2023-05-25 16:33:23 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,21 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [ -f "student-submission/ListExamples.java" ]
+then 
+    echo "The file ListExamples.java exists"
+else
+    echo "File not found"
+    echo "score 0 out of 4"
+    exit
+fi
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
