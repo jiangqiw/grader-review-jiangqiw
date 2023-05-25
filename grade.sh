7a82caf956c62cfd91c348e7e88be42b9c73de41
Jim Wu <jiw118@ucsd.edu>
2023-05-25 16:53:01 -0700

    update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,13 +8,6 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
-
 if [ -f "student-submission/ListExamples.java" ]
 then 
     echo "The file ListExamples.java exists"
@@ -31,4 +24,17 @@ cp -r lib grading-area
 cd grading-area
 
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if [[ $? -ne 0 ]]
+then
+    echo "There is a Compiler Error"
+    exit
+fi
+
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+# Draw a picture/take notes on the directory structure that's set up after
+# getting to this point
+
+# Then, add here code to compile and run, and do any post-processing of the
+# tests
